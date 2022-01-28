16b144ee113d532729506b11139f9ae7a75b062e
SCxoneZ <stevenniuschandraa@gmail.com>
2022-01-28 08:22:54 +0700

    beautifying the codes

--- script.js
+++ script.js
@@ -7,23 +7,27 @@ const wind = document.querySelector('.wind-value');
 const status = document.querySelector('.status-value');
 const api = {
   apikey: '681f35b64d6e9f95a76677364a4f78b0',
-  getData(city){
+  getData(city) {
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`).then(res => res.json()).then(data => data).catch(err => err)
   }
 }
 
 def();
 
-async function def(){
-  const data = await api.getData('Jakarta');
-  if(data.cod !== 200) return alert('Invalid');
-  input.value = 'Jakarta';
-  city.innerHTML = data.name;
-  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  degree.innerHTML = data.main.temp;
-  status.innerHTML = data.weather[0].main;
-  humidity.innerHTML = data.main.humidity;
-  wind.innerHTML = data.wind.speed;
+async function def() {
+  
+  try {
+    const data = await api.getData('Jakarta');
+    if (data.cod !== 200) return alert('Invalid');
+    input.value = 'Jakarta';
+    city.innerHTML = data.name;
+    img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    degree.innerHTML = data.main.temp;
+    status.innerHTML = data.weather[0].main;
+    humidity.innerHTML = data.main.humidity;
+    wind.innerHTML = data.wind.speed;
+  } catch (err) { console.log(err) }
+  
 }
 
 input.addEventListener('change', async () => {
